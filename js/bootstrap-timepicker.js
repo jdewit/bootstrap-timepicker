173bdb9261f3a15b6f55a86c7e6f71ee18b1d2da
Koen Punt <koenpunt@gmail.com>
2012-05-08 23:38:14 +0200

    fixed issue where hours could get below zero

--- js/bootstrap-timepicker.js
+++ js/bootstrap-timepicker.js
@@ -224,16 +224,16 @@
         }
 
         , decrementHour: function() {
-            if (this.hour === 1) {
-                if ( this.showMeridian ){
+            if ( this.showMeridian ){
+                if (this.hour === 1) {
                     this.hour = 12;
-                    this.toggleMeridian();
-                } else {
-                    this.hour = 0;
-                }
-            } else {
-                this.hour = this.hour - 1;
+                    return this.toggleMeridian();
+                } 
             }
+            if (this.hour === 0) {
+                return this.hour = 23;
+            }
+            this.hour = this.hour - 1;
         }
 
         , incrementMinute: function() {

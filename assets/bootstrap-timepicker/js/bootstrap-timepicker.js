a19a59f0318f88667aac66c60ad289476cee3956
Joris de Wit <joris.w.dewit@gmail.com>
2012-07-13 14:21:48 -0700

    fix meridian toggle on hour decrement

--- assets/bootstrap-timepicker/js/bootstrap-timepicker.js
+++ assets/bootstrap-timepicker/js/bootstrap-timepicker.js
@@ -243,7 +243,7 @@
                 if ( this.hour === 11 ) {
                     this.toggleMeridian();
                 } else if ( this.hour === 12 ) {
-                    this.hour = 1;
+                    return this.hour = 1;
                 }
             }
             if ( this.hour === 23 ) {
@@ -255,9 +255,10 @@
         , decrementHour: function() {
             if ( this.showMeridian ) {
                 if (this.hour === 1) {
-                    this.hour = 12;
-                    return this.toggleMeridian();
-                } 
+                    return this.hour = 12;
+                } else if (this.hour === 12) {
+                    this.toggleMeridian();
+                }
             }
             if (this.hour === 0) {
                 return this.hour = 23;

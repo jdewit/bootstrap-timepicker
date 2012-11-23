6022d8ebcd8a935bad0521cca2ea01c3779cee4c
Olivier Louvignes <olivier@mg-crea.com>
2012-11-23 18:42:15 +0100

    Added contributor note

--- js/bootstrap-timepicker.js
+++ js/bootstrap-timepicker.js
@@ -13,6 +13,7 @@
  * Nek
  * Chris Martin
  * Dominic Barnes contact@dominicbarnes.us
+ * Olivier Louvignes @olouv
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

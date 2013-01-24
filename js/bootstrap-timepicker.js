e17c84b3dd440d645522271ef568a02ddcad6d59
Joris de Wit <joris.w.dewit@gmail.com>
2013-01-24 09:40:59 -0800

    Change license to MIT

--- js/bootstrap-timepicker.js
+++ js/bootstrap-timepicker.js
@@ -1,32 +1,13 @@
-/* =========================================================
- * bootstrap-timepicker.js
- * http://www.github.com/jdewit/bootstrap-timepicker
- * =========================================================
- * Copyright 2012
+/*!
+ * Timepicker Component for Twitter Bootstrap
  *
- * Created By:
- * Joris de Wit @joris_dewit
+ * Copyright 2013 Joris de Wit
  *
- * Contributions By:
- * Gilbert @mindeavor
- * Koen Punt info@koenpunt.nl
- * Nek
- * Chris Martin
- * Dominic Barnes contact@dominicbarnes.us
+ * Contributors https://github.com/jdewit/bootstrap-timepicker/graphs/contributors
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ========================================================= */
-
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 !function($) {
 
     "use strict"; // jshint ;_;

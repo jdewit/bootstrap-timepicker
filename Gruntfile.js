a0b3749478018bb86971ef5ce052160c63a16e4f
Joris de Wit <joris.w.dewit@gmail.com>
2013-10-02 23:38:15 -0700

    add note on how to update gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -125,5 +125,5 @@ module.exports = function(grunt) {
 
   grunt.registerTask('default', ['jshint', 'jasmine', 'less:dev']);
   grunt.registerTask('test', ['jasmine', 'jshint']);
-  grunt.registerTask('compile', ['jshint', 'jasmine', 'uglify', 'less:prod', 'gh-pages']);
+  grunt.registerTask('compile', ['jshint', 'jasmine', 'uglify', 'less:prod']);
 };

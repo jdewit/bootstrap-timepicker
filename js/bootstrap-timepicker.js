6322eb5ce6aae8382b06f9bf6477736b85f05332
Johannes Heimbach <j.heimbach@anymotion.de>
2014-05-21 14:53:23 +0200

    changed picker to widget

--- js/bootstrap-timepicker.js
+++ js/bootstrap-timepicker.js
@@ -623,7 +623,7 @@
       this.$widget.removeClass('timepicker-orient-top timepicker-orient-bottom timepicker-orient-right timepicker-orient-left');
 
       if (this.orientation.x !== 'auto') {
-        this.picker.addClass('datepicker-orient-' + this.orientation.x);
+        this.$widget.addClass('datepicker-orient-' + this.orientation.x);
         if (this.orientation.x === 'right') {
           left -= widgetWidth - width;
         }

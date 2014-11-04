19d073a8d90ab8724493cc2788b4eaedc070c5e6
Oren Ferrari <oren.ferrari@ge.com>
2014-11-04 12:45:06 -0800

    Remove unnecessary constant  used as parameter.

--- js/bootstrap-timepicker.js
+++ js/bootstrap-timepicker.js
@@ -8,7 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-(function($, window, document, undefined) {
+(function($, window, document) {
   'use strict';
 
   // TIMEPICKER PUBLIC CLASS DEFINITION

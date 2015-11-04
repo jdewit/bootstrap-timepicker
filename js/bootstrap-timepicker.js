76f46fa5c7e6b18d747d8e619707b4a35a12601c
Justin Nichols <justin@nichols.mobi>
2015-11-04 13:48:00 -0500

    Timepicker will now close its widget when tab out
    If the timepicker widget is showing when the user
    tabs out of the input control (either via
    SHIFT+TAB or just TAB), the widget was still
    showing on the screen.  This commit fixes it so
    that the widget will close if it is open at the
    time the user tabs out.

--- js/bootstrap-timepicker.js
+++ js/bootstrap-timepicker.js
@@ -174,10 +174,16 @@
       case 9: //tab
         if (e.shiftKey) {
           if (this.highlightedUnit === 'hour') {
+            if (this.isOpen) {
+              this.hideWidget();
+            }
             break;
           }
           this.highlightPrevUnit();
         } else if ((this.showMeridian && this.highlightedUnit === 'meridian') || (this.showSeconds && this.highlightedUnit === 'second') || (!this.showMeridian && !this.showSeconds && this.highlightedUnit ==='minute')) {
+          if (this.isOpen) {
+            this.hideWidget();
+          }
           break;
         } else {
           this.highlightNextUnit();
